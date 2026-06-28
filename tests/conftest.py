9c046345058ee8b010783badc26e1d15911584cf
sinisaos <sinisaos@gmail.com>
2026-06-28 11:19:38 +0200

    revert SQLiteEngine changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,4 @@
 import asyncio
-import os
-import threading
 
 from piccolo.engine.finder import engine_finder
 
@@ -40,10 +38,6 @@ async def drop_tables():
         )
 
 
-def sqlite_cleanup(status):
-    os._exit(status)
-
-
 def pytest_sessionstart(session):
     """
     Make sure all the tables have been dropped, just in case a previous test
@@ -60,8 +54,3 @@ def pytest_sessionfinish(session, exitstatus):
     https://docs.pytest.org/en/latest/reference.html#_pytest.hookspec.pytest_sessionfinish
     """
     print("Session finishing")
-
-    if ENGINE.engine_type == "sqlite":
-        # Shutting down the SQLite process
-        t = threading.Timer(0.5, sqlite_cleanup, args=[exitstatus])
-        t.start()
